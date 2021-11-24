cc66b5c6b66b5bcf9ddacfdef52125c4dfcb42b3
Oh Donggeon <donggeon.dev@gmail.com>
2021-11-24 20:14:37 +0900

    GreedyFlorist.swift 주석추가

--- AlgorithmSwift/HackerRank/Greedys/GreedyFlorist.swift
+++ AlgorithmSwift/HackerRank/Greedys/GreedyFlorist.swift
@@ -1,13 +1,18 @@
 //
 //  GreedyFlorist.swift
-//  Algori thmSwift
+//  AlgorithmSwift
 //
 //  Created by Oh Donggeon on 2021/11/24.
 //
 
 import Foundation
 
-// Complete the getMinimumCost function below.
+/// Hacker rank - Greedy florist 욕심쟁이 문제 솔루션
+///
+/// - Date: 2021/11/24
+/// - Author:  Oh Donggeon
+/// - Link: https://www.hackerrank.com/challenges/greedy-florist
+///
 func getMinimumCost(k: Int, c: [Int]) -> Int {
     var count = 0
     return c.sorted(by: >).reduce(0) {
