d08b3da964fd68ba337f740b8652ecb595a94be4
Oh Donggeon <donggeon.dev@gmail.com>
2021-11-28 22:40:53 +0900

    구현중인 LinkedList 코드 추가 및 WordTransformation.swift 파일 추가

---
+++ AlgorithmSwift/DataStructure/LinkedList.swift
@@ -0,0 +1,65 @@
+//
+//  LinkedList.swift
+//  AlgorithmSwift
+//
+//  Created by Oh Donggeon on 2021/11/28.
+//
+
+import Foundation
+
+class Node<T> {
+    var value: T
+    var next: Node?
+    
+    init(_ value: T, _ next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+class LinkedList<T> {
+    private(set) var head: Node<T>?
+    private(set) var tail: Node<T>?
+    
+    var count: Int {
+        var result = 0
+        var node = head
+        
+        while node != nil {
+            result += 1
+            node = node?.next
+        }
+        
+        return result
+    }
+    
+    var values: [T] {
+        var node = head
+        var result: [T] = []
+        
+        while node != nil {
+            guard let unwrappedNode = node else { break }
+            
+            result.append(unwrappedNode.value)
+            node = unwrappedNode.next
+        }
+        
+        return result
+    }
+    
+    func addFirst(_ value: T) {
+        let node = Node(value)
+        
+        if tail == nil { tail = node }
+        
+        node.next = head
+        head = node
+    }
+    
+    func removeLast() -> T? {
+        if head === tail { head = nil }
+        
+        defer { tail = nil }
+        return tail?.value
+    }
+}
---
+++ AlgorithmSwift/Programmers/BFSAndDFS/WordTransformation.swift
@@ -0,0 +1,14 @@
+//
+//  WordTransformation.swift
+//  AlgorithmSwift
+//
+//  Created by Oh Donggeon on 2021/11/28.
+//
+
+import Foundation
+
+class WordTransformation {
+    
+    
+    
+}
