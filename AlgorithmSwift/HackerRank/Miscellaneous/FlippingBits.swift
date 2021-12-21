239cf08cdd4a44253ae4a2bcc67c3c7c680bd4ce
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-21 21:55:15 +0900

    해커랭크 flipping bits 솔루션 추가

---
+++ AlgorithmSwift/HackerRank/Miscellaneous/FlippingBits.swift
@@ -0,0 +1,18 @@
+//
+//  FlippingBits.swift
+//  AlgorithmSwift
+//
+//  Created by Oh Donggeon on 2021/12/21.
+//
+
+import Foundation
+
+/// 해커랭크 Flipping bits 솔루션
+///
+/// - Date: 2021/12/21
+/// - Author: Oh Donggeon
+/// - Link: https://www.hackerrank.com/challenges/flipping-bits
+///
+func flippingBits(n: Int) -> Int {
+    return ~n
+}
