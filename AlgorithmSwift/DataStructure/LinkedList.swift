c49b128f0d3e842c3fb03bbf1c3efa9eabe47c97
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-27 23:07:29 +0900

    연결리스트에서 단방향 연결리스트로 클래스 이름 수정

--- AlgorithmSwift/DataStructure/LinkedList.swift
+++ AlgorithmSwift/DataStructure/LinkedList.swift
@@ -11,58 +11,60 @@ import Foundation
 ///
 /// LinkedList 가 제대로 구현 되었는지 검증이 필요함
 ///
-class Node<T> {
-    var value: T
-    var next: Node?
+class SinglyNode<T> {
     
-    init(_ value: T, _ next: Node? = nil) {
+    var value: T
+    var next: SinglyNode?
+
+    init(_ value: T, _ next: SinglyNode? = nil) {
         self.value = value
         self.next = next
     }
+    
 }
 
 class LinkedList<T> {
-    private(set) var head: Node<T>?
-    private(set) var tail: Node<T>?
-    
+    private(set) var head: SinglyNode<T>?
+    private(set) var tail: SinglyNode<T>?
+
     var count: Int {
         var result = 0
         var node = head
-        
+
         while node != nil {
             result += 1
             node = node?.next
         }
-        
+
         return result
     }
-    
+
     var values: [T] {
         var node = head
         var result: [T] = []
-        
+
         while node != nil {
             guard let unwrappedNode = node else { break }
-            
+
             result.append(unwrappedNode.value)
             node = unwrappedNode.next
         }
-        
+
         return result
     }
-    
+
     func addFirst(_ value: T) {
-        let node = Node(value)
-        
+        let node = SinglyNode(value)
+
         if tail == nil { tail = node }
-        
+
         node.next = head
         head = node
     }
-    
+
     func removeLast() -> T? {
         if head === tail { head = nil }
-        
+
         defer { tail = nil }
         return tail?.value
     }
